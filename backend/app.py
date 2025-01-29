ca0312384f497b454b29fb7da078fb129013842f
yohannesalex <yohannesalemayehu624@gmail.com>
2025-01-29 16:15:51 +0300

    fixing the port number for deployment

--- backend/app.py
+++ backend/app.py
@@ -199,4 +199,7 @@ def generate_response():
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+    # app.run(debug=True)
